a8f438cb76c9ebcc9bb84c553de5059da9136aa1
SAYED HANZALA <162194668+Sayedhanzala777@users.noreply.github.com>
2025-11-07 23:17:08 +0530

    Add SQL queries for student data retrieval

--- day5.sql/day5.sql
+++ day5.sql/day5.sql
@@ -20,3 +20,25 @@ LIMIT 3;
 SELECT * FROM students
 ORDER BY age ASC
 LIMIT 2;
+-- 1. Marks descending
+SELECT * FROM students
+ORDER BY marks DESC;
+
+-- 2. Age ascending
+SELECT * FROM students
+ORDER BY age ASC;
+
+-- 3. Grade B, marks ascending
+SELECT * FROM students
+WHERE grade = 'B'
+ORDER BY marks ASC;
+
+-- 4. Top 3 students by marks
+SELECT * FROM students
+ORDER BY marks DESC
+LIMIT 3;
+
+-- 5. Youngest 2 students
+SELECT * FROM students
+ORDER BY age ASC
+LIMIT 2;
